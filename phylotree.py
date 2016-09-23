d9d57ba680217e5d6bf0f5565e4af8bc79c6452e
Nicolai von Kügelgen <nicolai.von-kuegelgen@student.uni-tuebingen.de>
2016-09-23 16:08:31 +0200

    Adapted Argparser to unified scripts and added Cluster mode

--- phylotree.py
+++ phylotree.py
@@ -35,8 +35,6 @@ class TreeMaker(object):
 		#                          A          B          C         A+B        A+C        B+C        all        none
 		self.standardcolors = ['#AA0000', '#00AA00', '#55AAFF', '#DDDD00', '#AA00AA', '#3333FF', '#000000', '#c8c8c8']
 
-		self.colors = self.standardcolors
-
 		# data should be read into self.elements (dict)
 
 		# These functions should be called (in order) at the end of the init() of each *child* class
@@ -285,10 +283,12 @@ class TreeMaker(object):
 
 class Combinations(TreeMaker):
 	def __init__(self, config, attr, **kwargs):
-		TreeMaker.__init__(**kwargs)
+		TreeMaker.__init__(self, **kwargs)
 
 		self.config = config
 		self.attr = attr
+
+		self.colors = self.standardcolors
 
 		# Read tree attributes into dict
 		self.elements = {}
@@ -533,10 +533,12 @@ class Combinations(TreeMaker):
 
 class Crosshits(TreeMaker):
 	def __init__(self, combo, bins, **kwargs):
-		TreeMaker.__init__(**kwargs)
+		TreeMaker.__init__(self, **kwargs)
 
 		self.combo = combo
 		tf = TaxFinder()
+
+		self.colors = self.standardcolors
 
 		# Read tree attributes into dict
 		self.elements = {}
@@ -550,7 +552,6 @@ class Crosshits(TreeMaker):
 
 		# self.featurelist list strs with feature names to be used (f_0, f_1, ...)
 		# the features of the top-level class (self.f_0, ...) equal a set-range of corresponding evalues (exponents only)
-		# TODO: add exception/fix if number of features is bigger then self.colors
 
 		# add Support for complete custum e-value ranges:
 		if isinstance(bins, set):
@@ -572,6 +573,10 @@ class Crosshits(TreeMaker):
 			# extend last range up to bins[1], more than last one may be missing due to rounding
 			lastf = getattr(self, self.featurelist[-1])
 			lastf.update(range(bins[1] - steps, bins[1] + 1))
+
+		# If not enough colors are given
+		while len(self.colors) < len(self.featurelist):
+			self.colors += self.colors
 
 		self.featurenames = ['e-values between 1e-{} and 1e-{}'.format(min(getattr(self, f)), max(getattr(self, f))) for f in self.featurelist]
 
@@ -701,23 +706,197 @@ class Crosshits(TreeMaker):
 
 # end Crosshits class
 
+
+class Clusters(TreeMaker):
+	def __init__(self, filename, dropclusters, **kwargs):
+		TreeMaker.__init__(self, **kwargs)
+
+		self.loadfile = filename
+		self.dontuse = dropclusters
+
+		self.colors = self.standardcolors
+
+		# Read tree attributes & clusters from file
+		# self.featurelist list strs with feature names to be used (f_0, f_1, ...)
+		# the features of the top-level class (self.f_0, ...) poinst to a list with the accession ids for this cluster
+
+		self.elements = {}
+		self.featurelist = []
+
+		with open(self.loadfile, 'r') as f:
+			i = 0
+			for line in f:
+				line = line.rstrip().split('#')[0]
+				if not line:
+					continue
+
+				i = i+1
+				if i in self.dontuse:
+					continue
+
+				cluster = line.split(',')
+				for entry in cluster:
+					tax = int(entry.split('^')[1])
+					if tax in self.elements:
+						self.elements[tax] += ['f_{}'.format(i)]
+					else:
+						self.elements[tax] = ['f_{}'.format(i)]
+
+				self.featurelist.append('f_{}'.format(i))
+				setattr(self, 'f_{}'.format(i), [n.split('^')[0] for n in cluster])
+
+		# for f in self.featurelist:
+		# 	print(f, getattr(self, f))
+
+		# If not enough colors are given
+		while len(self.colors) < len(self.featurelist):
+			self.colors += self.colors
+
+		self.featurenames = ['Cluster Nr: {}'.format(f[2:]) for f in self.featurelist]
+
+		self.addFeatures()
+
+		self.readPruningFile()
+
+		self.pruneTree()
+
+		self.treeLayout()
+
+	# end __init__()
+
+
+	def layout(self, node):
+		try:
+			percents = [round(100.0 * getattr(node, f) / node.total) for f in self.featurelist] + [round(100.0 * node.f_none / node.total)]
+		except ZeroDivisionError:
+			txt = TextFace(' ' + node.plainName, ftype='Arial', fgcolor='#000000')
+		else:
+			if sum(percents) != 100:
+				x = sorted(percents)[-1]
+				i = percents.index(x)
+				percents[i] += 100 - sum(percents)
+
+			if self.sqrt:
+				size = int(math.sqrt(node.total) * 5 + 1)
+			else:
+				size = int(math.log10(node.total) * 20 + 10)
+
+			P = PieChartFace(percents, size, size, self.colors)
+			faces.add_face_to_node(P, node, 0, position="branch-top")
+
+			fgcol = '#000000'
+
+			if self.count:
+				cnt = node.total
+				txt = TextFace(' {}\n {:.2f}'.format(node.plainName, cnt), ftype='Arial', fgcolor=fgcol)
+			else:
+				txt = TextFace(' ' + node.plainName, ftype='Arial', fgcolor=fgcol)
+
+		if node.is_leaf():
+			faces.add_face_to_node(txt, node, 0, position="branch-right")
+		else:
+			faces.add_face_to_node(txt, node, 0, position="branch-bottom")
+
+	# end layout
+
+
+	def treeLayout(self):
+		# Add a tree style and a legend
+
+		self.ts = TreeStyle()
+		self.ts.show_scale = False
+		self.ts.show_leaf_name = False
+		self.ts.extra_branch_line_type = 0
+		self.ts.extra_branch_line_color = 'black'
+		self.ts.layout_fn = self.layout
+
+		self.ts.legend_position = 4
+
+		for name, color in zip(self.featurenames, self.colors):
+			self.ts.legend.add_face(CircleFace(10, color), column=0)
+			self.ts.legend.add_face(TextFace(name), column=1)
+
+		# This should use a different color
+		if self.empty:
+			self.ts.legend.add_face(CircleFace(10, self.colors[7]), column=0)
+			self.ts.legend.add_face(TextFace('none'), column=1)
+
+		self.ts.legend.add_face(TextFace(datetime.now().strftime('%a, %d.%m.%Y; %H:%M:%S'), fsize=8), column=1)
+		self.ts.legend.add_face(TextFace('Tree: ' + self.treefile, fsize=8), column=1)
+		self.ts.legend.add_face(TextFace('Clusters: ' + self.loadfile, fsize=8), column=1)
+		self.ts.legend.add_face(TextFace('Pruning: ' + self.prune, fsize=8), column=1)
+
+		# Order the graphical tree output from highly branched nodes on top and less branched nodes to the bottom
+		self.t.ladderize(1)
+
+	# end treeLayout
+
+
+	def addFeatures(self):
+		# Traverse the tree and add features to each node
+		for n in self.t.traverse('postorder'):
+			if n.name == 'NoName' or not n.name:
+				n.name = 'Biota^1'
+
+			n.add_features(plainName=n.name.split('^')[0])
+			n.add_features(taxid=int(n.name.split('^')[1]))
+
+			kwargs = {f: 0 for f in self.featurelist}
+			n.add_features(**kwargs)
+
+			n.add_features(f_none=0)
+			n.add_features(total=0)
+
+			if n.is_leaf():
+				countThis = True
+				i = 0
+				try:
+					# Tree is derived from blast hits, number of promters is lower, self.elements doesnt have values for all tax-ids
+					features = self.elements[n.taxid].copy()
+
+					while features:
+						f = features.pop()
+						i += 1
+						setattr(n, f, getattr(n, f) + 1)
+
+				except KeyError:
+					# No match in self.elements
+					if self.empty:
+						n.f_none += 1
+						i += 1
+					else:
+						countThis = False
+
+				if countThis:
+					n.total += i
+
+			else:
+				for x in n.children:
+					for f in self.featurelist:
+						# n.'f' += x.'f'
+						setattr(n, f, getattr(n, f) + getattr(x, f))
+
+					n.f_none += x.f_none
+					n.total += x.total
+
+	# end addFeatures()
+
+# end Clusters class
+
+
 if __name__ == '__main__':
 	import argparse
 
-	parser = argparse.ArgumentParser(description='Create a tree with piecharts. You need: Newick tree file, config file, attributes file, and a pruning file')
+	parser = argparse.ArgumentParser(description='Create a tree with piecharts. Different modes are supported: *combination* of proteins, *crosshit* e-value distribution, Protein *cluster* distributons.', add_help=False)
 
-	parser.add_argument('-m', '--mode', default='combinations', choices=['combinations', 'crosshits', 'MSAclusters'],
-						help='Chose the mode / kind of Tree you want to make. Options: combinations, crosshits, MSAclusters')
+	subparsers = parser.add_subparsers(title='Modes', description='Use phylotree.py <mode> -h for a full help with all flags/arguments', dest='mode')
 
-	# TODO: Use the GroupSystem of Argparser to make this behave properly
-	# Options for 'Combinations'
-	parser.add_argument('-c', '--config', default='tree_config.txt', help='Filename of the file that contains the config with the tree features and the colors.')
-	parser.add_argument('-a', '--attr', default='attributes.txt', help='Filename of the file with the tree attributes.')
+	parser_co = subparsers.add_parser('combination', aliases=['com'], help='Show which given feature/protein combinations are present for each node.')
+	parser_cr = subparsers.add_parser('crosshit', aliases=['cross'], help='Show the e-value distribuiton over a given range for each node')
+	parser_cl = subparsers.add_parser('cluster', aliases=['cl'], help='Show distribuiton of proteins/promoters from different clusters')
 
-	# Options for 'Crosshits'
-	parser.add_argument('combo', help='Filename of .tsv file with the crosshits of two proteins.')
-	parser.add_argument('-b', '--bins', type=int, default=[5], nargs=1, help='Number of bins/features to auto-creation of e-value ranges')
-	parser.add_argument('-r', '--ranges', type=int, nargs='+', default=[30,150], help='Min. 2 values. Either lower & upper bounds for ranges autocalculation, or multiple range borders')
+	# Custom help, to include all arguments for subparsers
+	parser.add_argument('-h', '--help', action='store_true', help='show this help message and exit')
 
 	# General options
 	parser.add_argument('-t', '--tree', default='trees/general.tre', help='Filename of .tre file for the tree. The file must be in Newick format and the names in the format: name^taxid')
@@ -731,17 +910,71 @@ if __name__ == '__main__':
 	parser.add_argument('-w', '--width', type=int, default=1000, help='Width of the resulting picture in pixels.')
 	parser.add_argument('--count_total', action='store_true', help='Print the total number of hits in addition to Piechart size.')
 
+	# Options only for 'combination'
+	parser_co.add_argument('-c', '--config', default='tree_config.txt', help='Filename of the file that contains the config with the tree features and the colors.')
+	parser_co.add_argument('-a', '--attr', default='attributes.txt', help='Filename of the file with the tree attributes.')
+
+	# Options only for 'crosshit'
+	parser_cr.add_argument('proteins', help='Filename of the .tsv file with the crosshits of two proteins.')
+	parser_cr.add_argument('-b', '--bins', type=int, default=[5], nargs=1, help='Number of bins/features to auto-creation of e-value ranges')
+	parser_cr.add_argument('-r', '--ranges', type=int, nargs='+', default=[30,150], help='Min. 2 values. Either lower & upper bounds for ranges autocalculation, or multiple range borders')
+
+	# Options only for 'cluster'
+	parser_cl.add_argument('clusters', help='Filename of the .csv file that contains one cluster per line.')
+	parser_cl.add_argument('-d', '--dropclusters', nargs='+', type=int, default=[], help='Indices of clusters that should not be included (First index = 1).')
+
 	args = parser.parse_args()
+
+	#Generation of custom help message, that includes options & Usage for all modes
+	if args.help:
+		helps = {}
+		usages = {}
+		mainhelp = parser.format_help()
+
+		# Get help-str & usage from all subarersers (hard-coded into list)
+		for p, m in zip([parser_co, parser_cr, parser_cl], ['combination', 'crosshit', 'cluster']):
+			# This only works if ALL subparsers have a small number of options so the 'usage' info is in one line
+			helpl = p.format_help().split('\n')
+			helpstr = '\n'.join(helpl[2:])
+			helps[m] = helpstr
+			usages[m] = helpl[0]
+
+		#Add custom usage
+		olduse = mainhelp.split('{')[0]
+		olduse = olduse.split('\n')
+
+		newusel = olduse[:-1]
+		for m, subuse in usages.items():
+			newusel += [olduse[-1] + '{{{}}}'.format(m) + subuse.split('[-h]')[1]]
+
+		print('\n'.join(newusel) + '\n')
+
+		#add general arguments
+		print('Create a tree' + mainhelp.split('Create a tree')[1].split('Modes:')[0])
+		#print('Generally useable arguments (to be added before {mode}):\n')
+
+		# Insert modes & respective arguments
+		# Abbreviations hard-coded into list
+		for m, s in zip(sorted(helps.keys()), sorted(['com', 'cross', 'cl'])):
+			print('arguments usable after {{{0} ({1})}}:\n'.format(m, s))
+			print('\n'.join([l for l in helps[m].split('\n') if '--help' not in l]) + '\n')
+
+		sys.exit()
+
 
 	t = Tree(args.tree, format=8)
 
-	generalargs ={'tree': t, 'treefile': args.tree, 'prune': args.prune, 'sqrt': args.sqrt, 'collapse': not args.nocollapse, 'show_empty_species': args.empty, 'startnode': args.startnode, 'countTotal': args.count_total}
+	generalargs = {'tree': t, 'treefile': args.tree, 'prune': args.prune, 'sqrt': args.sqrt, 'collapse': not args.nocollapse, 'show_empty_species': args.empty, 'startnode': args.startnode, 'countTotal': args.count_total}
 
-	if args.mode == 'combinations':
+	if not args.mode:
+		print('Giving the mode argument is required. See --help for more information. \nValid modes are: combination (com), crosshit (cross)')
+		sys.exit()
+
+	elif args.mode.startswith('com'):
 		extraargs = {'config': args.config, 'attr': args.attr}
 		useTree = Combinations
 
-	elif args.mode == 'crosshits':
+	elif args.mode.startswith('cross'):
 		if len(args.ranges) == 2:
 			bins = args.ranges + args.bins
 		elif len(args.ranges) > 2:
@@ -750,12 +983,12 @@ if __name__ == '__main__':
 			print('ranges option (-r) needs at least two values!')
 			sys.exit()
 
-		extraargs = {'combo': args.combo, 'bins': bins}
+		extraargs = {'combo': args.proteins, 'bins': bins}
 		useTree = Crosshits
 
-	else:
-		print('Not a valid mode. Valid options are: combinations, crosshits, MSAclusters')
-		sys.exit()
+	elif args.mode.startswith('cl'):
+		extraargs = {'filename': args.clusters, 'dropclusters': args.dropclusters}
+		useTree = Clusters
 
 	allargs = {**generalargs, **extraargs}
 	TM = useTree(**allargs)
